7ce3bbf9ef4988dc7eb9b84e1fdbc83660f29ea4
yfaria <yuriluisfaria@gmail.com>
2021-07-20 16:22:51 -0300

    creating code for the web api of wql and changing dependencies

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,53 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import SparqlGenerator
+import           Web.Spock
+import           Web.Spock.Config
 
---main :: IO String
+import           Data.Aeson        hiding (json)
+import           Data.Monoid       ((<>))
+import           Data.Text         (Text, pack, unpack)
+import           GHC.Generics
+
+import           SparqlGenerator   (generateOptSPARQL)
+import Database.HSparql.Connection (selectQueryRaw)
+
+data Person = Person
+  { name :: Text
+  , age  :: Int
+  } deriving (Generic, Show)
+
+newtype EntWql = EntWql
+  { q :: Text
+  } deriving (Generic, Show)
+
+instance ToJSON Person
+
+instance FromJSON Person
+
+instance ToJSON EntWql
+
+instance FromJSON EntWql
+
+type Api = SpockM () () () ()
+
+type ApiAction a = SpockAction () () () a
+
+main :: IO ()
 main = do
-  s <- getLine
-  putStrLn $ generateSPARQL s
+  spockCfg <- defaultSpockCfg () PCNoDatabase ()
+  runSpock 8080 (spock spockCfg app)
+
+app :: Api
+app = do
+  get "people" $ do
+    json $ Person { name = "Fry", age = 25 }
+  post "people" $ do
+    thePerson <- jsonBody' :: ApiAction Person
+    text $ "Parsed: " <> pack (show thePerson)
+  post "query" $ do
+    thePerson <- jsonBody' :: ApiAction EntWql
+    -- let x = selectQueryRaw "http://TURASTATION:10035/repositories/gold-erg" (generateOptSPARQL "[ARG3 x]")
+    text $ pack (generateOptSPARQL $ unpack $ q thePerson)
